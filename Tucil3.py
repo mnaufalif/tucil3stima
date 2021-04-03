faf0dafb1d05185272a52da7b56504b1c73dfec0
Almeiza Arvin Muzaki <79525918+almeizaarvin@users.noreply.github.com>
2021-04-03 10:40:14 +0700

    Add files via upload

--- Tucil3.py
+++ Tucil3.py
@@ -1,13 +1,27 @@
-from flask import Flask, redirect, url_for, render_template
+import os
+from werkzeug.utils import secure_filename
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__, template_folder="Templates")
 
-
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def home():
+    thenodes=[]
+    i=0
+    while(i<3):
+        thenodes+=[getnode()]
+        i+=1
+        render_template("Home.html")
+    print(thenodes)
     return render_template("Home.html")
+
+
+def getnode():
+    if request.method=="POST" :
+        return request.form["n"]
 
 #///////////////////////////////
 
 if __name__ =="__main__":
     app.run()
+
